89e9e8adee487a87f475e464f7a2d858f808395c
zhou839 <3648569497@qq.com>
2025-01-11 03:58:59 +0800

    Add files via upload

---
+++ zhou中心1(1) (1).lua
@@ -0,0 +1,27 @@
+game:GetService("StarterGui"):SetCore("SendNotification",{ Title = "『zhou』"; Text ="作者:zhou"; Duration = 5; })
+local ui = loadstring(game:HttpGet("https://raw.githubusercontent.com/dingding123hhh/hun/main/jmlibrary1.lua"))();        
+local win = ui:new("zhou")
+--
+local UITab3 = win:Tab("『111』",'7734068321')
+
+local about = UITab3:section("『111』",true)
+
+about:Button("jm",function()
+getgenv().lishichuan="禁漫中心1001390385" loadstring(game:HttpGet("https://raw.githubusercontent.com/dingding123hhh/anlushanjinchangantangwanle/main/jmjmjmjmjmjmjmjmjmjmjmjmjmjmjmjm.lua"))()
+end)
+
+about:Button("皮脚本",function()
+getgenv().HeJaoBen="鹤脚本QQ群951177681" loadstring(game:HttpGet("https://raw.githubusercontent.com/qazpin662/11/refs/heads/main/%E6%A3%80%E6%B5%8B.lua"))()
+end)
+
+local UITab35 = win:Tab("『222』",'7734068321')
+
+local about = UITab35:section("『222』",true)
+
+about:Button("冷脚本",function()
+loadstring(game:HttpGet("https://pastebin.com/raw/iZuasZCc"))()
+end)
+
+about:Button("xk脚本中心", function()
+loadstring(game:HttpGet("loadstring("\108\111\97\100\115\116\114\105\110\103\40\103\97\109\101\58\72\116\116\112\71\101\116\40\34\104\116\116\112\115\58\47\47\114\97\119\46\103\105\116\104\117\98\117\115\101\114\99\111\110\116\101\110\116\46\99\111\109\47\66\73\78\106\105\97\111\98\122\120\54\47\66\73\78\106\105\97\111\47\109\97\105\110\47\88\75\46\84\88\84\34\41\41\40\41\10")()"))()
+end）
